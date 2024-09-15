abf41fecc02d7d0b19aa5f19ffe568c9817e10d1
Aleix Amils <aleix.amils@cirvianum.cat>
2024-09-15 21:52:44 +0200

    Primera pujada del projecte Hola Món amb JS

---
+++ script.js
@@ -0,0 +1,9 @@
+function saluda() {
+    document.getElementById('missatge').textContent = "Hola món! Has clicat el botó!";
+    
+}
+
+// Exemple output per consola
+// function saluda() {
+//     console.log("Hola món! Has clicat el botó!");
+// }
